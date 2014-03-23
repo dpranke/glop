5ad94f0812d5448635808ad39d7f82ee4d30880a
Dirk Pranke <dpranke@chromium.org>
2014-03-23 14:38:27 -0700

    refactor main

--- main.py
+++ main.py
@@ -14,18 +14,17 @@ def main(host, argv=None):
         return 1
 
     try:
-        g, err = build_grammar(host, args)
+        grammar_txt, grammar_fname, err = grammar_from_args(host, args)
         if err:
             host.print_err(err)
             return 1
 
-        input_txt, input_fname, err = build_input(host, args)
+        input_txt, input_fname, err = input_from_args(host, args)
         if err:
             host.print_err(err)
             return 1
 
-        interp = GrammarInterp(g, input_txt, input_fname)
-        out, err = interp.parse()
+        out, err = parse(grammar_txt, input_txt, grammar_fname, input_fname)
         if err:
             host.print_err(err)
         if out:
@@ -38,39 +37,6 @@ def main(host, argv=None):
     except KeyboardInterrupt:
         host.print_err('Interrupted, exiting ..')
         return 130  # SIGINT
-
-
-def build_grammar(host, args):
-    if args.grammar_cmd:
-        grammar_fname = '<-c>'
-        grammar_txt = args.grammar_cmd
-    else:
-        grammar_fname = args.grammar_file
-        if not host.exists(grammar_fname):
-            return None, 'grammar file "%s" not found' % grammar_fname
-        grammar_txt = host.read(grammar_fname)
-
-    g_parser = OMParser(grammar_txt, grammar_fname)
-    g_ast, err = g_parser.parse()
-    if err:
-        return None, err
-
-    return GrammarAnalyzer(g_ast).analyze()
-
-
-def build_input(host, args):
-    if args.input_cmd:
-        input_fname = '<cmd>'
-        input_txt = args.input_cmd
-    elif args.files:
-        input_fname = args.files[0]
-        if not host.exists(input_fname):
-            return None, None, 'input file "%s" not found' % input_fname
-        input_txt = host.read(input_fname)
-    else:
-        input_fname = '<stdin>'
-        input_txt = host.stdin.read()
-    return input_txt, input_fname, None
 
 
 def parse_args(argv):
@@ -86,6 +52,44 @@ def parse_args(argv):
     arg_parser.add_argument('files', nargs='*', default=[],
                             help=argparse.SUPPRESS)
     return arg_parser.parse_args(argv)
+
+
+def grammar_from_args(host, args):
+    if args.grammar_cmd:
+        grammar_fname = '<-c>'
+        grammar_txt = args.grammar_cmd
+    else:
+        grammar_fname = args.grammar_file
+        if not host.exists(grammar_fname):
+            return None, None, 'grammar file "%s" not found' % grammar_fname
+        grammar_txt = host.read(grammar_fname)
+    return grammar_txt, grammar_fname, None
+
+
+def input_from_args(host, args):
+    if args.input_cmd:
+        input_fname = '<cmd>'
+        input_txt = args.input_cmd
+    elif args.files:
+        input_fname = args.files[0]
+        if not host.exists(input_fname):
+            return None, None, 'input file "%s" not found' % input_fname
+        input_txt = host.read(input_fname)
+    else:
+        input_fname = '<stdin>'
+        input_txt = host.stdin.read()
+    return input_txt, input_fname, None
+
+
+def parse(grammar_txt, input_txt, grammar_fname='', input_fname=''):
+    g_parser = OMParser(grammar_txt, grammar_fname)
+    g_ast, err = g_parser.parse()
+    if err:
+        return None, err
+
+    g = GrammarAnalyzer(g_ast).analyze()
+    interp = GrammarInterp(g, input_txt, input_fname)
+    return interp.parse()
 
 
 if __name__ == '__main__':
