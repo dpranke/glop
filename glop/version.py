a5289a44fa519d607707113e0698554688595850
Dirk Pranke <dpranke@google.com>
2024-11-10 17:01:03 -0800

    restructure version code a bit

--- glop/version.py
+++ glop/version.py
@@ -12,4 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-VERSION = '0.8.0'
+__version__ = '0.8.1'
+
+# For backward-compatibility with earlier versions of glop:
+VERSION = __version__
